38f1c1f56e77123381975beef2de28b9e69a6895
Brandamonium <brandamonium@gmail.com>
2021-11-01 23:20:30 -0400

    Basic react app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,8 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
-  return (
-   <div>Hello World</div>
-  );
+  return <div>Nope </div>;
 }
 
 export default App;
